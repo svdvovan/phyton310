83851e932d9707f4e047e4bfbc528af8b0130ac8
SretenskyVD <svdvovan@gmail.com>
2022-04-20 13:12:50 +0300

    доделал парсер для АЛИ

--- genetrip2Page.py
+++ genetrip2Page.py
@@ -7,9 +7,9 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 root = Tk()
-root.geometry('850x200+1000+300')
+root.geometry('850x220+1000+300')
 
-fileName = 'genetrip2.xlsx'
+# fileName = 'genetrip2.xlsx'
 
 l_url = Label(root, text="Введите url страницы категории:").grid(row=0, column=0, pady=10)
 e_url = Entry(root, width=100)
@@ -25,6 +25,12 @@ e_lastPage = Entry(root, width=10)
 e_lastPage.insert(0, "1")
 e_lastPage.grid(row=2, column=1, padx=15, sticky=W)
 
+
+l_catName = Label(root, text="Название категории:").grid(row=3, column=0, pady=10)
+e_catName = Entry(root, width=100)
+# e_catName.insert(0, "1")
+e_catName.grid(row=3, column=1, padx=15, sticky=W)
+
 excel_file = Workbook()
 excel_sheet = excel_file.create_sheet(title='genetrip2', index=0)
 
@@ -32,7 +38,7 @@ count = 2
 countRow = 2
 
 
-def bigParser(e_url, page, lastPage):
+def bigParser(e_url, page, lastPage, catName):
     global count
     global countRow
 
@@ -86,8 +92,11 @@ def bigParser(e_url, page, lastPage):
 
                         name = soup.find('h1', class_='h1-prod-name').getText()
                         mainFoto = soup.find('a', id="zoom1").get('href')
-                        # foto = soup.find('div', class_='image-additional owl-carousel').find_all('a',
-                        #                                                                          class_='thumbnail')
+                        try:
+                            foto = soup.find('div', class_='image-additional owl-carousel').find_all('a',
+                                                                                                 class_='thumbnail')
+                        except:
+                            foto =''
                         # category_for_post = soup.find('span', class_='posted_in').find('a').getText()
                         id = soup.find(itemprop="model").getText()
                         price = soup.find('span',
@@ -95,69 +104,75 @@ def bigParser(e_url, page, lastPage):
                         try:
                             manufac = soup.find(itemprop="brand").getText()
                         except:
-                            manufac = 'noBrand'
-                        finally:
-                            description1 = soup.find('div', id='tab-description')
-                            description = str(description1)
+                            manufac = ''
+
+                        description1 = soup.find('div', id='tab-description')
+                        description = str(description1)
+                        try:
                             svoistva = soup.find('table', class_='table table-bordered').find_all('td',
                                                                                                   itemprop='name')
 
                             svoistva_znach = soup.find('table', class_='table table-bordered').find_all('td',
                                                                                                         itemprop='value')
-                            print(id)
-                            print(manufac)
-                            print(description)
-                            print(price)
-                            print(name)
-                            #
-                            excel_sheet[f'A{count}'] = id
-                            excel_sheet[f'B{count}'] = name
-                            excel_sheet[f'C{count}'] = price
-                            # excel_sheet[f'D{count}'] = category_for_post
-                            excel_sheet[f'E{count}'] = description
-                            excel_sheet[f'F{count}'] = manufac
-                            excel_sheet[f'G{count}'] = mainFoto
+                        except:
+                            svoistva =''
+                            svoistva_znach =''
 
-                            # for f in foto:
-                            #     largeFoto = f['href']
-                            #     largeFoto2.append(largeFoto)
-                            # print(largeFoto2)
-                            #
-                            # for fot in largeFoto2:
-                            #     excel_sheet.cell(row=1, column=countCol).value = f'фото{countFoto}'
-                            #     excel_sheet.cell(row=countRow, column=countCol).value = fot
-                            #     countFoto += 1
-                            #     countCol += 1
+                        print(id)
+                        print(manufac)
+                        print(description)
+                        print(price)
+                        print(name)
+                        #
+                        excel_sheet[f'A{count}'] = id
+                        excel_sheet[f'B{count}'] = name
+                        excel_sheet[f'C{count}'] = price
+                        excel_sheet[f'D{count}'] = catName
+                        excel_sheet[f'E{count}'] = description
+                        excel_sheet[f'F{count}'] = manufac
+                        excel_sheet[f'G{count}'] = mainFoto
 
-                            datas = [i.get_text(strip=True) for i in svoistva]
-                            datas2 = [i.get_text() for i in svoistva_znach]
+                        for f in foto:
+                            largeFoto = f['href']
+                            largeFoto2.append(largeFoto)
+                        print(largeFoto2)
 
-                            for data in datas:
-                                excel_sheet.cell(row=1, column=CountPn).value = f'Свойство {CountP}'
-                                excel_sheet.cell(row=countRow, column=CountPn).value = data.replace(':', '')
-                                CountP += 1
-                                CountPn += 2
+                        for fot in largeFoto2:
+                            excel_sheet.cell(row=1, column=countCol).value = f'фото{countFoto}'
+                            excel_sheet.cell(row=countRow, column=countCol).value = fot
+                            countFoto += 1
+                            countCol += 1
 
-                            for data2 in datas2:
-                                excel_sheet.cell(row=1, column=CountPnZ).value = f'Значение {CountPZZ}'
-                                excel_sheet.cell(row=countRow, column=CountPnZ).value = data2
-                                CountPnZ += 2
-                                CountPZZ += 1
+                        datas = [i.get_text(strip=True) for i in svoistva]
+                        datas2 = [i.get_text() for i in svoistva_znach]
 
-                            count += 1
-                            countRow += 1
+                        for data in datas:
+                            excel_sheet.cell(row=1, column=CountPn).value = f'Свойство {CountP}'
+                            excel_sheet.cell(row=countRow, column=CountPn).value = data.replace(':', '')
+                            CountP += 1
+                            CountPn += 2
 
-                    excel_file.save(fileName)
+                        for data2 in datas2:
+                            excel_sheet.cell(row=1, column=CountPnZ).value = f'Значение {CountPZZ}'
+                            excel_sheet.cell(row=countRow, column=CountPnZ).value = data2
+                            CountPnZ += 2
+                            CountPZZ += 1
+
+                        count += 1
+                        countRow += 1
+
+                excel_file.save(f'1_{catName}.xlsx')
 
 
 def getUrl():
     urls = e_url.get()
+    catName = e_catName.get()
     page = int(e_page.get())
     # print(page)
     lastPage = int(e_lastPage.get())
     # print(lastPage)
     # print(urls)
-    bigParser(urls, page, lastPage)
+    bigParser(urls, page, lastPage, catName)
     #bigParser(urls)
 
 
